beb660cfe9382891d8d94ede43370b5f7991247b
Geordon Worley <vadixidav@gmail.com>
2018-01-08 01:47:15 -0500

    make random snakes

--- src/snake.rs
+++ src/snake.rs
@@ -1,8 +1,17 @@
-use gru;
-use af::{self, Array};
+use gru::GRU;
+use af::{self, Array, Dim4};
 
 pub struct Snake {
-    brain: gru::GRU,
+    brain: GRU,
     /// Dimension 1 has X values in position 0 and Y values in position 1.
     body: Array,
+}
+
+impl Snake {
+    pub fn new_rand() -> Snake {
+        Snake {
+            brain: GRU::new_rand(128, 128),
+            body: af::randu::<f32>(Dim4::new(&[1, 2, 1, 1])),
+        }
+    }
 }
